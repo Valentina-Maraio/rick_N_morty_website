42ddb1db044b9f023b73a7011af9a2d0e4468972
Valentina-Maraio <valentina.maraio89@gmail.com>
2024-06-11 17:29:47 +0200

    navbar style and routes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,32 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/global.css";
+import Marvel_Logo from "../assets/Marvel.svg";
+import Favs from "../assets/Fav_counter.png";
 
 const Navbar = () => {
   return (
     <>
-    <Link to='/'>Home</Link>
-    <Link to='/vista_principal'>Vista Principal</Link>
-    <Link to='/detalle_personaje'>Detalle Personaje</Link>
+      <div className="header">
+        <div className="logo">
+          <Link to="/">
+            <img
+              src={Marvel_Logo}
+              alt="Marvel Logo"
+              width="122.22"
+              height="44"
+            />
+          </Link>
+        </div>
+        <div className="favorites">
+          <Link to="/favorites">
+            <img src={Favs} alt="Marvel Logo" />
+            <span className="fav_counter">0</span>
+          </Link>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
