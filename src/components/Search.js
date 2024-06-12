d0a902c88bb2405c517b2f95d85eb01655d8e95e
Valentina-Maraio <valentina.maraio89@gmail.com>
2024-06-12 13:36:49 +0200

    context api setup, cards content

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import '../styles/search_bar.css';
+import input_logo from '../assets/Input_empty.png'
 
 const Search = () => {
   return (
     <>
-        <div>Search</div>
-        <input placeholder='search your hero'/>
+    <div className="input_container">
+      <input className="input_field" placeholder='SEARCH A CHARACTER...'/>
+    </div>
     </>
   )
 }
 
-export default Search
+export default Search;
